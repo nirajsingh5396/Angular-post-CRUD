8298810b070afe903661bdb82b37891bb0981f0c
niraj <niraj.kumar@plexcel.com>
2021-01-05 17:41:46 +0530

    Added routing for /post to get all post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: PostsComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full',
+  },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
